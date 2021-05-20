e4275eb17890c1fb7d8b2f525749e65562173111
icerazor <razor.89@mail.ru>
2021-05-20 16:35:49 +0300

    two commit

---
+++ zadanie.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+int i = 5;
+char c = 'B';
+float f = 2.12;
+double d =2.165656464566454646;
+int arr[] ={1,2,3};
+
+enum SYMBOLS {EMPTY='_',PLAYER='X',AI='0'};
+
+return 0;
+
+}
